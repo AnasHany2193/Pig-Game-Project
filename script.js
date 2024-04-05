00e12c7adee809747db8599799322500076d3cbc
Anas Hany <anashany2193@gmail.com>
2024-04-05 23:14:09 +0200

    implement the initial state

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
+
+// reset elemetns
+score0.textContent = 0;
+score1.textContent = 0;
+
+// Hide the dice
+dice.classList.add('hidden');
