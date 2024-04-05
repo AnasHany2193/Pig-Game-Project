2badd2d33830e75a37ab26ca00671f2bc55cf1d0
Anas Hany <anashany2193@gmail.com>
2024-04-05 23:17:00 +0200

    Update the initial state from jonasCode

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 'use strict';
 
 // Selecting elements
-const score0 = document.querySelector('#score--0');
-const score1 = document.querySelector('#score--1');
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
 const dice = document.querySelector('.dice');
 
-// reset elemetns
+// initial State
 score0.textContent = 0;
 score1.textContent = 0;
-
-// Hide the dice
 dice.classList.add('hidden');
