e4e3fa629b247caee82351e987c5f4e00d57c720
Anas Hany <anashany2193@gmail.com>
2024-04-06 02:33:45 +0200

    User holds dice

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ const current1 = document.getElementById('current--1');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 
-const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const dice = document.querySelector('.dice');
 
 // initial State
 score0.textContent = 0;
@@ -26,6 +28,18 @@ let scores = [0, 0];
 let activePlayer = 0;
 let currentScore = 0;
 
+// switch player function
+const swtichPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
+
 // --------- User rolls dice ---------
 btnRoll.addEventListener('click', function () {
   // Generate random dice roll
@@ -38,18 +52,34 @@ btnRoll.addEventListener('click', function () {
   // Check it dice is 1? Swtich player : add score && new score
   if (diceNum === 1) {
     // Swtich player
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-
-    activePlayer = activePlayer === 0 ? 1 : 0;
-
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
+    swtichPlayer();
   } else {
     // Add dice roll to Current Score
     currentScore += diceNum;
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
+  }
+});
+
+// --------- User holds dice ---------
+btnHold.addEventListener('click', function () {
+  // add current score to total score
+  scores[activePlayer] += currentScore;
+  console.log(scores[activePlayer]);
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // check if score >= 100? win player : switch player
+  if (scores[activePlayer] >= 100) {
+    // Current player Wins!
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    // Switch player
+    swtichPlayer();
   }
 });
